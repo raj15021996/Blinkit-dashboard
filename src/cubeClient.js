10085165979c753686f95904872e528a8abaf721
raj15021996 <rajlyf18@gmail.com>
2025-04-04 19:42:19 +0530

    update cube config file

--- src/cubeClient.js
+++ src/cubeClient.js
@@ -1,8 +1,16 @@
-import cubejs from '@cubejs-client/core';
+import cube from '@cubejs-client/core';
 
 // The correct way to initialize the Cube.js client with apiUrl in the configuration object
-const cubejsApi = cubejs({
-  apiUrl: process.env.NEXT_PUBLIC_CUBEJS_API_URL,
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_CUBEJS_API_TOKEN}`  
-});
+// const cubejsApi = cube({
+//   apiUrl: process.env.NEXT_PUBLIC_CUBEJS_API_URL,
+//     Authorization: `Bearer ${process.env.NEXT_PUBLIC_CUBEJS_API_TOKEN}`  
+// });
+
+const cubejsApi = cube(
+  process.env.NEXT_PUBLIC_CUBEJS_API_TOKEN,
+  {
+    apiUrl:process.env.NEXT_PUBLIC_CUBEJS_API_URL,
+  }
+);
+
 export default cubejsApi;
