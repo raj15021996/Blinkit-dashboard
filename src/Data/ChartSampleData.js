1b4536de85444a116c5fda4fc680493714d79366
raj15021996 <rajlyf18@gmail.com>
2025-04-04 19:29:54 +0530

    optimise the code

---
+++ src/Data/ChartSampleData.js
@@ -0,0 +1,39 @@
+export const sampleData = {
+    lineChartData : [
+        { day: 9, "This Month": 3.5, "Last Month": 2.8 },
+        { day: 10, "This Month": 9.2, "Last Month": 3.1 },
+        { day: 11, "This Month": 4.0, "Last Month": 3.4 },
+        { day: 12, "This Month": 8.5, "Last Month": 3.2 },
+        { day: 13, "This Month": 5.0, "Last Month": 3.6 },
+        { day: 14, "This Month": 4.7, "Last Month": 3.9 },
+        { day: 15, "This Month": 5.2, "Last Month": 4.1 },
+      ],
+    pieChartData: [
+        { name: 'New Delhi', value: 26.5, percentageChange: 1.2, percentageValue: 35 },
+        { name: 'Mumbai', value: 36.4, percentageChange: -3.3, percentageValue: 23 },
+        { name: 'West Bengal', value: 12.2, percentageChange: -2.3, percentageValue: 21 },
+        { name: 'Others', value: 24.3, percentageChange: 1.09, percentageValue: 9 },
+    ],
+    tableData: [
+      {
+        id: '1',
+        name: 'Protein Bar 100g',
+        sales: 953112.12,
+        outOfStock: 1.68,
+        totalInventory: 933.9,
+        averageRank: 3.2,
+        estTraffic: 12303,
+        estImpressions: 25005,
+      },
+      {
+        id: '2',
+        name: 'Choco Bar 100g',
+        sales: 48526.32,
+        outOfStock: 6.79,
+        totalInventory: 679,
+        averageRank: 7,
+        estTraffic: 3606,
+        estImpressions: 4231,
+      },
+    ],
+  };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,13 @@
 'use client';
-import React, { Suspense } from 'react';
+import React from 'react';
 import { CubeProvider } from "@cubejs-client/react";
 import cubejsApi from '@/cubeClient.js';
 import Dashboard from '@/components/Dashboard';
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading dashboard...</div>}>
       <CubeProvider cubeApi={cubejsApi}>
-      <Dashboard />
+        <Dashboard />
       </CubeProvider>
-    </Suspense>
   );
 }
